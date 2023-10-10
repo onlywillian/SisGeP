84efa9fa454af11684345ee764d9116a46cb72ed
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-10-10 17:47:56 -0300

    root layout and dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import AuthProvider from '@/contexts/authContext'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
