1e7adfab91a24b3657ee9234da05059d604ab107
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-10-10 17:47:18 -0300

    home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+export default async function Home() {
+    const response = await fetch("http://localhost:3001/home",{
+      cache: "no-cache"
+    });
+    const data = await response.json();     
+
+    console.log(data);
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <h1 className="text-white text-3xl mb-20 font-bold">Visão Geral</h1>
@@ -6,15 +12,15 @@ export default function Home() {
       <div className="w-full flex gap-8 justify-center">
         <div className="bg-white w-40 p-4">
           <h1 className="font-bold mb-4">Equipamentos</h1>
-          <p className="">Quantidade: x</p>
+          <p className="">Quantidade: {data.Equipments}</p>
         </div>
         <div className="bg-white w-40 p-4">
           <h1 className="font-bold mb-4">Espaços</h1>
-          <p className="">Quantidade: x</p>
+          <p className="">Quantidade: {data.Locations}</p>
         </div>
         <div className="bg-white w-40 p-4">
           <h1 className="font-bold mb-4">Usuários</h1>
-          <p className="">Quantidade: x</p>
+          <p className="">Quantidade: {data.Users}</p>
         </div>
       </div>
     </div>
